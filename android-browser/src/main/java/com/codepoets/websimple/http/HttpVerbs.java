93ce1f4632b4ef1ad3935b01140b3bf591ec6d3b
Nic Strong <nic.strong@gmail.com>
2011-08-11 22:41:36 +1200

    Removed code moved to new packages android-websimple & slf4j-android

--- android-browser/src/main/java/com/codepoets/websimple/http/HttpVerbs.java
+++
@@ -1,9 +0,0 @@
-package com.codepoets.websimple.http;
-
-public class HttpVerbs
-{
-	public static final String GET = "GET";
-	public static final String POST = "POST";
-	public static final String PUT = "PUT";
-	public static final String HEAD = "HEAD";
-}
